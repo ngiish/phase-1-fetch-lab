dc8717ba25ae8dd0b16754a5ced744a64a3d4bd4
Isaac <govegeku00@gmail.com>
2023-10-06 13:57:59 +0300

    Initial Attempt

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
+
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+};
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -10,7 +13,7 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
-}
+};
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
